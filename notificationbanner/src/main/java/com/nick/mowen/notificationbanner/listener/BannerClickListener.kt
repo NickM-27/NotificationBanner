24091d4d3a673ebb70e92e6f91e805c753ca5d3f
Nick Mowen <nickmowen213@gmail.com>
2018-07-09 10:59:14 -0500

    Added more documentation

--- notificationbanner/src/main/java/com/nick/mowen/notificationbanner/listener/BannerClickListener.kt
+++ notificationbanner/src/main/java/com/nick/mowen/notificationbanner/listener/BannerClickListener.kt
@@ -1,9 +1,16 @@
 package com.nick.mowen.notificationbanner.listener
 
 import android.view.View
+import com.nick.mowen.notificationbanner.view.NotificationBanner
 import com.nick.mowen.notificationbanner.data.BannerInfo
 
 interface BannerClickListener {
 
+    /**
+     * Listener to react to banner click action
+     *
+     * @param view [NotificationBanner] that was clicked
+     * @param info [BannerInfo] that was shown
+     */
     fun onClick(view: View, info: BannerInfo)
 }
